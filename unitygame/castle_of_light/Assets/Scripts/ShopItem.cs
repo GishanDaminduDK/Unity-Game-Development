22f10ad688ccff4d717b7953e012f0017223a6a6
Nusrath Amana <nusrathamana526@gmail.com>
2024-05-17 23:30:11 +0530

    updating shop

--- unitygame/castle_of_light/Assets/Scripts/ShopItem.cs
+++ unitygame/castle_of_light/Assets/Scripts/ShopItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[CreateAssetMenu(fileName = "ShopMenu", menuName = "ScriptableObjects/New shop item",order = 1 )]
+
 public class ShopItem : ScriptableObject
 {
     public Sprite itemImg;
@@ -11,6 +11,4 @@ public class ShopItem : ScriptableObject
     public int price;
     public string currencyTxt;
     public Sprite currencyImg;
-
-
 }
---
+++ unitygame/unity_project/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShopItem : ScriptableObject
+{
+    public Sprite itemImg;
+    public string title;
+    public string description;
+    public int price;
+    public string currencyTxt;
+    public Sprite currencyImg;
+}
