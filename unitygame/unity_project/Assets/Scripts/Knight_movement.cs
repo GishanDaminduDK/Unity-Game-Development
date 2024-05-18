2283f7922e65f4a3175f8ea47d6529f7864fbbb7
Gishandamindu <gishankalasinghe1999@gmail.com>
2024-05-18 23:32:43 +0530

    updated

--- unitygame/unity_project/Assets/Scripts/Knight_movement.cs
+++ unitygame/unity_project/Assets/Scripts/Knight_movement.cs
@@ -7,7 +7,21 @@ public class Knight_movement : MonoBehaviour
     private Rigidbody2D rb;
     private float moveH, moveV;
     [SerializeField] private float moveSpeed = 1.0f;
+    private bool isPaused = false; // Track whether the game is paused
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        LoadPosition();  // Load the position at the start
+    }
+    private void Update()
+    {
+        if (!isPaused) // Only process input if the game is not paused
+        {
+            moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+        }
 
+    }
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,6 +29,8 @@ public class Knight_movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (!isPaused)
+        { 
         moveH = Input.GetAxis("Horizontal")*moveSpeed;
         moveV = Input.GetAxis("Vertical")*moveSpeed;
         rb.velocity = new Vector2(moveH, moveV);
@@ -22,9 +38,52 @@ public class Knight_movement : MonoBehaviour
         Vector2 direction = new Vector2(moveH, moveV);
 
         FindObjectOfType<Knight_animation>().SetDirection(direction);
+        }
+
+    }
+    public void SavePosition()
+    {
+        GameData data = new GameData
+        {
+            playerPositionX = transform.position.x,
+            playerPositionY = transform.position.y,
+            coinscount = itemCollector.coins
+        };
+
+        string json = JsonUtility.ToJson(data);
+        Debug.Log("Saving Data: " + json); // Check what is being saved
+        PlayerPrefs.SetString("PlayerPosition", json);
+        PlayerPrefs.Save();
     }
 
-   
+    private void LoadPosition()
+    {
+        if (PlayerPrefs.HasKey("PlayerPosition"))
+        {
+            string json = PlayerPrefs.GetString("PlayerPosition");
+            GameData data = JsonUtility.FromJson<GameData>(json);
+            Debug.Log("Loaded Data: " + json); // Check what is being loaded
+            Vector2 position = new Vector2(data.playerPositionX, data.playerPositionY);
+            rb.position = position;
+            itemCollector.coins = data.coinscount; // Make sure to restore the coins count
+        }
+    }
+
+    public void PauseAndSaveGame()
+    {
+        isPaused = true;  // Set the game as paused
+        Debug.Log("Check when pause is true");
+        SavePosition();
+        Time.timeScale = 0; // Pause the game
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;  // Set the game as not paused
+        Debug.Log("Check when pause is flase");
+        Time.timeScale = 1; // Resume the game
+    }
+
 
 
 
